4110bba2640ef24498fdee23edda7790fe5a43d1
Sercan KAYA <sercan.kaya@icloud.com>
2021-09-05 16:31:55 +0300

    finish app

--- lib/app/clients/shortly_clients.dart
+++ lib/app/clients/shortly_clients.dart
@@ -3,28 +3,37 @@ import 'package:shortly_flutter/core/network/interfaces/base_client_generator.da
 
 part 'shortly_clients.freezed.dart';
 
+// * Tüm istekler burdan gitmektedir. Tüm parametre ayarları buradan yapılmaktadır.
+// * const factory ShortlyClients.posts({Map<String, dynamic>? queryParameters}) = _Posts;
+// * örnekteki gibi isimlendirmeler gibi istekler buradan ayarlanıp istekler gönderilebilir.
+
 @freezed
 class ShortlyClients extends BaseClientGenerator with _$ShortlyClients {
   const ShortlyClients._() : super();
   const factory ShortlyClients.posts({Map<String, dynamic>? queryParameters}) =
       _Posts;
 
+// * istekler için base url;
   @override
   String get baseURL => "https://api.shrtco.de/v2/";
 
+// * Eğer bodu gönderilecekse buradan ayarlanır. Aksi belirtilmez ise null olur.
   @override
   Map<String, dynamic>? get body {
     return this.maybeWhen(orElse: () => null);
   }
 
+// * header alanı buradan ayarlanır.
   @override
   Map<String, dynamic> get header => {"Content-Type": "application/json"};
 
+// * İsteğin methodu buradan gönderilir.
   @override
   String get method {
     return this.maybeWhen<String>(orElse: () => "GET");
   }
 
+// * Base url'e eklenen path'dir.
   @override
   String get path {
     return this.when<String>(
@@ -32,6 +41,7 @@ class ShortlyClients extends BaseClientGenerator with _$ShortlyClients {
     );
   }
 
+// * Query parametreleri buradan gönderilir.
   @override
   Map<String, dynamic>? get queryParameters {
     return this.maybeWhen(
